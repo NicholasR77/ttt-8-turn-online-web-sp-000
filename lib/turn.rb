e136ed0b44adb999c2bee511d23ae99050012f6d
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 18:05:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-display_board(test_board)
+#display_board(test_board)
+
+#convert input to index
+def input_to_index(input)
+  index = input.to_i
+end
 
 #validate move method
 def valid_move?(index)
