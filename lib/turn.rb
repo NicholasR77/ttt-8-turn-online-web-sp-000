8a09e53b806d8a584777c27e4186ea5d308d7674
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 17:47:13 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,12 @@
+#ask for input
+
+#get input
+
+#convert input to ibndex
+
+#if index is validation
+  #make the move for index
+  #show the board
+#else
+  #ask for input again until valid input
+#end
