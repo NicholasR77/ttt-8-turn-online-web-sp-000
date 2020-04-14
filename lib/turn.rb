fc8e93ba0b5ad20eee69124586d850531cb771fc
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 18:21:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -62,7 +62,9 @@ def turn(board)
   puts ("Please enter 1-9:")
   input = gets.strip
   index = input_to_index(input)
-  puts index
+  if (valid_move?(index) == true && position_taken?(board, index) == true)
+    puts "This is a valid move"
+  end
 end
 
 #move the piece method
