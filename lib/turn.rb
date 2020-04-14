55b30c11451f3579b23d847242905a92a803833a
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-14 18:28:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -68,8 +68,10 @@ def turn(board)
     puts "A token already exists there. Please enter a new move."
   else
     puts "This is not a valid move. Please enter a new move."
-    input = gets.strip
-    index = input_to_index(input)
+    until (valid_move?(index) == true && position_taken?(board, index) == false)
+      input = gets.strip
+      index = input_to_index(input)
+    end
   end
 end
 
